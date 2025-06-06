2780ab52488f6adeeeff5c3c903e685bf3322579
Aziz K <aziz.kamara@outlook.com>
2025-06-06 15:08:00 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const fetchFakeData = async () => {
+    const response = await fetch('http://localhost:3000/fakeDataSet');
+    const fakeData = await response.json();
+    console.log(fakeData);
+  }
+
+  useEffect(() => {
+    fetchFakeData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
